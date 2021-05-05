f41b93792e6ad1a8bb01d3a7b0b8a4c55d8f6d44
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-05-05 23:01:33 +0300

    Project Files Updated
    -- Old Files DELETED!

--- SQLite/app/src/main/java/com/omercankoc/sqlite/ListViewActivity.kt
+++ SQLite/app/src/main/java/com/omercankoc/sqlite/ListViewActivity.kt
@@ -7,5 +7,6 @@ class ListViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
+
     }
 }
